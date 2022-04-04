315424c4478ee191662389895ecb46b4365702a0
sanjukumar1 <sarojkumar.bit@gmail.com>
2022-04-04 12:57:02 +0530

    3rd commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 ## library MASS is used to get inverse of squared as well as non squared matrix
 library(MASS)
 makeCacheMatrix <- function(x = matrix()) {
